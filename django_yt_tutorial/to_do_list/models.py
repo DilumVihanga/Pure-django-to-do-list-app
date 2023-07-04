15e9203ebd09f498eb7aed8489e8d5d7f49a36b9
Dilum Vihanga <88120903+DilumVihanga@users.noreply.github.com>
2023-07-04 19:48:12 +0530

    model created and makemigrations

--- django_yt_tutorial/to_do_list/models.py
+++ django_yt_tutorial/to_do_list/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Tasks(models.Model):
+    
+    title = models.CharField(max_length=200)
+    date = models.DateField(null=True)
+    time = models.TimeField(null=True)
