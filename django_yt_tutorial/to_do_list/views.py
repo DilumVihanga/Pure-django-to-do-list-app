7dfa7d3dcafe80dc31320d77b5de725ea6e544cf
Dilum Vihanga <88120903+DilumVihanga@users.noreply.github.com>
2023-07-04 15:01:56 +0530

    csrf token error

--- django_yt_tutorial/to_do_list/views.py
+++ django_yt_tutorial/to_do_list/views.py
@@ -3,7 +3,20 @@ from django.http import HttpResponse
 
 def home(request):
     """ return HttpResponse("<h1>This is the home page</h1>") """
-    return render(request, "home.html", {})
+    my_tasks = list()
+
+    my_tasks = list()
+
+    if request.method == "POST":
+        task = request.POST.get("task")
+        my_tasks.append(task)
+
+    context = {
+        "tasks": my_tasks,
+    }
+
+    return render(request, 'home.html', context)
+
 
 def login(request):
     """ return HttpResponse("<h1>This is the login page</h1>") """
